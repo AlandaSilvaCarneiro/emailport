8312948972a00e6a6794e5f2be58ce533d276f1f
AlandaSilvaCarneiro <82735697+AlandaSilvaCarneiro@users.noreply.github.com>
2024-03-06 14:28:16 -0300

    init

---
+++ src/main/java/br/com/java/port/emailport/EmailportApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.port.emailport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/port/emailport/EmailportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.port.emailport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
